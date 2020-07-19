1e0d79602eb7333ee63ae26889a82ed31fb95850
Higor Denomar <higordenomar@hotmail.com>
2020-07-19 18:12:41 -0300

    removed api files

--- src/api/example.js
+++
@@ -1,8 +0,0 @@
-const Insta = require('./index'); 	
-const insta = new Insta('user','password');
-
-async function test(){
-   await insta.auth(); 
-   const res  =  await insta.getFullFollowers().then(e=>console.log(e)); 
-}
-test()
